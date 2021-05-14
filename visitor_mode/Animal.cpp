540286a2279ed768ffd98edef27c8fbc6d71e1c8
Yonghao Shu <123shuyonghao@gmail.com>
2021-05-13 22:54:35 -0700

    Visitor mode raw example

---
+++ visitor_mode/Animal.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+class Animal {
+public:
+    void speak()
+    {
+        printf("emm... \n");
+    }
+};
+
+class Cat : public Animal {
+public:
+    void speak()
+    {
+        printf("Meow...\n");
+    }
+};
+
+class Dog : public Animal {
+public:
+    void speak()
+    {
+        printf("Bark...\n");
+    }
+};
+
+class Fox : public Animal {
+public:
+    void speak()
+    {
+        printf("Owooooo...\n");
+    }
+};
+
+class Speaker {
+public:
+    void speak(Animal *obj)
+    {
+        obj->speak();
+    }
+};
+
+int main() {
+    Animal *a = new Dog();
+    Animal *b = new Cat();
+    Animal *c = new Fox();
+    Speaker *s = new Speaker();
+    s->speak(a);
+    s->speak(b);
+    s->speak(c);
+}
